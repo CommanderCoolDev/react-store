a63328785226cdf96c637bf362ba2248cb666b22
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2022-06-05 15:27:28 +0300

    added links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ function Header() {
   return (
     <nav className="teal accent-4">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="#">Repo</a>
+            <a href="https://github.com/CommanderCoolDev/react-store">Repo</a>
           </li>
         </ul>
       </div>
