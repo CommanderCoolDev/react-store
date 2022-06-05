2709aec137182d79e4b124867b30235cbd3de5a5
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2022-06-05 15:28:55 +0300

    fix links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,8 @@ function Footer() {
           <a
             className="grey-text text-lighten-4 right"
             href="https://github.com/CommanderCoolDev/react-store"
+            target="_blank"
+            rel="noreferrer"
           >
             Repo
           </a>
